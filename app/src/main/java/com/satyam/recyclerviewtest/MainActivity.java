6358984d0bce503d79a4e9fa85fba81e015c042f
Satyam Prajapati <satyam7355pra@gmail.com>
2022-04-01 23:41:35 +0530

    Add data functionality Added

--- app/src/main/java/com/satyam/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/satyam/recyclerviewtest/MainActivity.java
@@ -4,32 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
-    EditText editText;
-    Button button;
-ArrayList<Model> data =new ArrayList<>();
+    FloatingActionButton btn_open_dialog;
+    ProgrammingAdapter adapter;
+    String str;
+    ArrayList<Model> data = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn_open_dialog = findViewById(R.id.btn_open_dialog);
         recyclerView = findViewById(R.id.recycleList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         data.add(new Model("satyam"));
         data.add(new Model("Java"));
         data.add(new Model("Android"));
         data.add(new Model("Developer"));
-        ProgrammingAdapter adapter=new ProgrammingAdapter(data);
+        data.add(new Model("Developer1"));
+        data.add(new Model("Developer2"));
+        data.add(new Model("Developer3"));
+        adapter = new ProgrammingAdapter(data);
         recyclerView.setAdapter(adapter);
+        btn_open_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.add_update_layout);
+                EditText etName=dialog.findViewById(R.id.et_name);
+                Button btnAdd=dialog.findViewById(R.id.btnAdd);
+                btnAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        data.add(new Model(etName.getText().toString()));
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+
+            }
+        });
+       
 
 
     }
--- app/src/main/java/com/satyam/recyclerviewtest/ProgrammingAdapter.java
+++ app/src/main/java/com/satyam/recyclerviewtest/ProgrammingAdapter.java
@@ -1,8 +1,10 @@
 package com.satyam.recyclerviewtest;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,24 +12,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.ProgrammingViewHolder> {
-ArrayList<Model> data;
+
+    ArrayList<Model> data;
 
     public ProgrammingAdapter(ArrayList<Model> data) {
         this.data = data;
     }
 
-    public ProgrammingAdapter() {
-    }
 
     @Override
-    public ProgrammingViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.items,parent,false);
+    public ProgrammingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items, parent, false);
         return new ProgrammingViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder( ProgrammingAdapter.ProgrammingViewHolder holder, int position) {
-     holder.name.setText(data.get(position).name);
+    public void onBindViewHolder(ProgrammingAdapter.ProgrammingViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.name.setText(data.get(position).name);
+        holder.btnImgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data.remove(position);
+                notifyItemRemoved(position);
+            }
+        });
 
     }
 
@@ -36,12 +44,15 @@ ArrayList<Model> data;
         return data.size();
     }
 
-    class ProgrammingViewHolder extends RecyclerView.ViewHolder{
-TextView name;
-        public ProgrammingViewHolder( View itemView) {
+    class ProgrammingViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView btnImgDelete;
+        public ProgrammingViewHolder(View itemView) {
             super(itemView);
-            name =itemView.findViewById(R.id.name);
+            name = itemView.findViewById(R.id.name);
+            btnImgDelete=itemView.findViewById(R.id.btnImgDelete);
+
         }
     }
- 
+
 }
